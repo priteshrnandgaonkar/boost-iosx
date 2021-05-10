0140fead7dc2a83ced50540d9ae0f573d45654cd
Pritesh Nandgaonkar <prit91@fb.com>
2021-05-10 14:01:36 +0100

    update artifacts with arm64 support for sim

--- scripts/build.sh
+++ scripts/build.sh
@@ -39,7 +39,7 @@ if [ ! -f boost/b2 ]; then
 	popd
 fi
 
-############### ICU
+# ############### ICU
 if [ ! -d $SCRIPT_DIR/Pods/icu4c-iosx/product ]; then
 	pushd $SCRIPT_DIR
 	pod install --verbose
@@ -47,7 +47,7 @@ if [ ! -d $SCRIPT_DIR/Pods/icu4c-iosx/product ]; then
 	mkdir $SCRIPT_DIR/Pods/icu4c-iosx/product/lib
 fi
 ICU_PATH=$SCRIPT_DIR/Pods/icu4c-iosx/product
-############### ICU
+# ############### ICU
 
 pushd boost
 
@@ -76,8 +76,10 @@ fi
 patch tools/build/src/build/configure.jam $SCRIPT_DIR/configure.jam.patch
 fi
 
-#LIBS_TO_BUILD="--with-locale"
-LIBS_TO_BUILD="--with-atomic --with-chrono --with-container --with-context --with-contract --with-coroutine --with-date_time --with-exception --with-fiber --with-filesystem --with-graph --with-iostreams --with-json --with-locale --with-log --with-math --with-nowide --with-program_options --with-random --with-regex --with-serialization --with-stacktrace --with-system --with-test --with-thread --with-timer --with-type_erasure --with-wave"
+#LIBS_TO_BUILD="--with-locale "
+# LIBS_TO_BUILD="--with-atomic --with-chrono --with-container --with-context --with-contract --with-coroutine --with-date_time --with-exception --with-fiber --with-filesystem --with-graph --with-iostreams --with-json --with-locale --with-log --with-math --with-nowide --with-program_options --with-random --with-regex --with-serialization --with-stacktrace --with-system --with-test --with-thread --with-timer --with-type_erasure --with-wave"
+
+LIBS_TO_BUILD="--with-context --with-filesystem --with-program_options --with-regex --with-system --with-thread"
 
 B2_BUILD_OPTIONS="release link=static runtime-link=shared define=BOOST_SPIRIT_THREADSAFE"
 
@@ -98,6 +100,7 @@ cp $ICU_PATH/frameworks/icudata.xcframework/macos-$HOST_ARC/libicudata.a $ICU_PA
 cp $ICU_PATH/frameworks/icui18n.xcframework/macos-$HOST_ARC/libicui18n.a $ICU_PATH/lib/
 cp $ICU_PATH/frameworks/icuuc.xcframework/macos-$HOST_ARC/libicuuc.a $ICU_PATH/lib/
 ./b2 -j8 --stagedir=stage/macosx cxxflags="-std=c++17" -sICU_PATH="$ICU_PATH" toolset=darwin address-model=64 architecture=$BOOST_ARC $B2_BUILD_OPTIONS $LIBS_TO_BUILD
+# ./b2 -j8 --stagedir=stage/macosx cxxflags="-std=c++17" toolset=darwin address-model=64 architecture=$BOOST_ARC $B2_BUILD_OPTIONS $LIBS_TO_BUILD
 rm -rf bin.v2
 fi
 
@@ -105,6 +108,8 @@ if true; then
 if [[ -f tools/build/src/user-config.jam ]]; then
 	rm -f tools/build/src/user-config.jam
 fi
+echo "MACOS work"
+
 cat >> tools/build/src/user-config.jam <<EOF
 using darwin : catalyst : clang++ -arch $HOST_ARC --target=$BOOST_ARC-apple-ios13-macabi -isysroot $MACSYSROOT/SDKs/MacOSX.sdk -I$MACSYSROOT/SDKs/MacOSX.sdk/System/iOSSupport/usr/include/ -isystem $MACSYSROOT/SDKs/MacOSX.sdk/System/iOSSupport/usr/include -iframework $MACSYSROOT/SDKs/MacOSX.sdk/System/iOSSupport/System/Library/Frameworks
 : <striper> <root>$MACSYSROOT
@@ -115,6 +120,8 @@ cp $ICU_PATH/frameworks/icudata.xcframework/ios-$HOST_ARC-maccatalyst/libicudata
 cp $ICU_PATH/frameworks/icui18n.xcframework/ios-$HOST_ARC-maccatalyst/libicui18n.a $ICU_PATH/lib/
 cp $ICU_PATH/frameworks/icuuc.xcframework/ios-$HOST_ARC-maccatalyst/libicuuc.a $ICU_PATH/lib/
 ./b2 -j8 --stagedir=stage/catalyst cxxflags="-std=c++17" -sICU_PATH="$ICU_PATH" toolset=darwin-catalyst address-model=64 architecture=$BOOST_ARC $B2_BUILD_OPTIONS $LIBS_TO_BUILD
+# ./b2 -j8 --stagedir=stage/catalyst cxxflags="-std=c++17" toolset=darwin-catalyst address-model=64 architecture=$BOOST_ARC $B2_BUILD_OPTIONS $LIBS_TO_BUILD
+
 rm -rf bin.v2
 fi
 
@@ -122,6 +129,8 @@ if true; then
 if [[ -f tools/build/src/user-config.jam ]]; then
 	rm -f tools/build/src/user-config.jam
 fi
+echo "Building for iPhoneOS"
+
 cat >> tools/build/src/user-config.jam <<EOF
 using darwin : ios : clang++ -arch arm64 -fembed-bitcode-marker -isysroot $DEVSYSROOT/SDKs/iPhoneOS.sdk
 : <striper> <root>$DEVSYSROOT
@@ -132,6 +141,9 @@ cp $ICU_PATH/frameworks/icudata.xcframework/ios-arm64/libicudata.a $ICU_PATH/lib
 cp $ICU_PATH/frameworks/icui18n.xcframework/ios-arm64/libicui18n.a $ICU_PATH/lib/
 cp $ICU_PATH/frameworks/icuuc.xcframework/ios-arm64/libicuuc.a $ICU_PATH/lib/
 ./b2 -j8 --stagedir=stage/ios cxxflags="-std=c++17" -sICU_PATH="$ICU_PATH" toolset=darwin-ios address-model=64 instruction-set=arm64 architecture=arm binary-format=mach-o abi=aapcs target-os=iphone define=_LITTLE_ENDIAN define=BOOST_TEST_NO_MAIN $B2_BUILD_OPTIONS $LIBS_TO_BUILD
+# ./b2 -j8 --stagedir=stage/ios cxxflags="-std=c++17" toolset=darwin-ios address-model=64 instruction-set=arm64 architecture=arm binary-format=mach-o abi=aapcs target-os=iphone define=_LITTLE_ENDIAN define=BOOST_TEST_NO_MAIN $B2_BUILD_OPTIONS $LIBS_TO_BUILD
+# ./b2 -j8 --stagedir=stage/ios architecture=combined cxxflags="-std=c++17 -arch arm64 -arch armv7 -arch armv7s -isysroot $DEVSYSROOT/SDKs/iPhoneOS.sdk" toolset=clang define=_LITTLE_ENDIAN binary-format=mach-o abi=aapcs define=BOOST_TEST_NO_MAIN $B2_BUILD_OPTIONS $LIBS_TO_BUILD
+
 rm -rf bin.v2
 fi
 
@@ -139,18 +151,67 @@ if true; then
 if [[ -f tools/build/src/user-config.jam ]]; then
 	rm -f tools/build/src/user-config.jam
 fi
-cat >> tools/build/src/user-config.jam <<EOF
-using darwin : iossim : clang++ -arch $HOST_ARC -fembed-bitcode-marker -isysroot $SIMSYSROOT/SDKs/iPhoneSimulator.sdk
-: <striper> <root>$SIMSYSROOT
-: <architecture>$BOOST_ARC <target-os>iphone
-;
-EOF
-cp $ICU_PATH/frameworks/icudata.xcframework/ios-$HOST_ARC-simulator/libicudata.a $ICU_PATH/lib/
-cp $ICU_PATH/frameworks/icui18n.xcframework/ios-$HOST_ARC-simulator/libicui18n.a $ICU_PATH/lib/
-cp $ICU_PATH/frameworks/icuuc.xcframework/ios-$HOST_ARC-simulator/libicuuc.a $ICU_PATH/lib/
-./b2 -j8 --stagedir=stage/iossim cxxflags="-std=c++17" -sICU_PATH="$ICU_PATH" toolset=darwin-iossim address-model=64 architecture=$BOOST_ARC target-os=iphone define=BOOST_TEST_NO_MAIN $B2_BUILD_OPTIONS $LIBS_TO_BUILD
+
+echo "Building artifacts for iPhoneSimulator"
+# cat >> tools/build/src/user-config.jam <<EOF
+# using darwin : iossim : clang++ -arch $HOST_ARC -arch arm64 -fembed-bitcode-marker -isysroot $SIMSYSROOT/SDKs/iPhoneSimulator.sdk
+# : <striper> <root>$SIMSYSROOT
+# ;
+# EOF
+# using darwin : iossim : clang++ -arch arm64 -fembed-bitcode-marker -isysroot $SIMSYSROOT/SDKs/iPhoneSimulator.sdk
+# : <striper> <root>$SIMSYSROOT
+# : <architecture>arm <target-os>iphone
+# # : <architecture> $BOOST_ARC <target-os>iphone
+# : <architecture>$BOOST_ARC <target-os>iphone
+
+# cp $ICU_PATH/frameworks/icudata.xcframework/ios-$HOST_ARC-simulator/libicudata.a $ICU_PATH/lib/
+# cp $ICU_PATH/frameworks/icui18n.xcframework/ios-$HOST_ARC-simulator/libicui18n.a $ICU_PATH/lib/
+# cp $ICU_PATH/frameworks/icuuc.xcframework/ios-$HOST_ARC-simulator/libicuuc.a $ICU_PATH/lib/
+# ./b2 -j8 --stagedir=stage/iossim cxxflags="-std=c++17" -sICU_PATH="$ICU_PATH" toolset=darwin-iossim address-model=64 architecture=$BOOST_ARC target-os=iphone define=BOOST_TEST_NO_MAIN $B2_BUILD_OPTIONS $LIBS_TO_BUILD
+./b2 -j8 --stagedir=stage/iossim architecture=combined cxxflags="-std=c++17 -arch i386 -arch arm64 -arch x86_64 -isysroot $SIMSYSROOT/SDKs/iPhoneSimulator.sdk" toolset=clang define=_LITTLE_ENDIAN binary-format=mach-o abi=aapcs define=BOOST_TEST_NO_MAIN $B2_BUILD_OPTIONS $LIBS_TO_BUILD
+
 rm -rf bin.v2
 fi
+
+
+# rm -rf arm64 x86_64 universal
+# ./bootstrap.sh --with-toolset=clang --with-libraries=thread,system,filesystem,program_options,serialization
+# ./b2  cxxflags="-arch arm64" toolset=darwin -a
+# mkdir -p arm64 && cp stage/lib/*.dylib arm64
+# ./b2 toolset=clang cxxflags="-arch arm64 -arch x86_64" -a
+# mkdir x86_64 && cp stage/lib/*.dylib x86_64
+# mkdir universal
+# for dylib in arm64/*; do
+#   lipo -create -arch arm64 $dylib -arch x86_64 x86_64/$(basename $dylib) -output universal/$(basename $dylib);
+# done
+
+
+
+
+# if true; then
+# if [[ -f tools/build/src/user-config.jam ]]; then
+# 	rm -f tools/build/src/user-config.jam
+# fi
+
+# echo "iPhoneSimulator work arm"
+# cat >> tools/build/src/user-config.jam <<EOF
+# using darwin : iossim : clang++ -arch arm64 -fembed-bitcode-marker -isysroot $SIMSYSROOT/SDKs/iPhoneSimulator.sdk
+# : <striper> <root>$SIMSYSROOT
+# : <architecture>arm <target-os>iphone
+# ;
+# EOF
+# echo "finised iPhoneSimulator work"
+
+# # : <architecture> $BOOST_ARC <target-os>iphone
+
+# # cp $ICU_PATH/frameworks/icudata.xcframework/ios-$HOST_ARC-simulator/libicudata.a $ICU_PATH/lib/
+# # cp $ICU_PATH/frameworks/icui18n.xcframework/ios-$HOST_ARC-simulator/libicui18n.a $ICU_PATH/lib/
+# # cp $ICU_PATH/frameworks/icuuc.xcframework/ios-$HOST_ARC-simulator/libicuuc.a $ICU_PATH/lib/
+# # ./b2 -j8 --stagedir=stage/iossim cxxflags="-std=c++17" -sICU_PATH="$ICU_PATH" toolset=darwin-iossim address-model=64 architecture=$BOOST_ARC target-os=iphone define=BOOST_TEST_NO_MAIN $B2_BUILD_OPTIONS $LIBS_TO_BUILD
+# ./b2 -j8 --stagedir=stage/iossimarm cxxflags="-std=c++17" toolset=darwin-iossim address-model=64 architecture=arm target-os=iphone binary-format=mach-o abi=aapcs define=_LITTLE_ENDIAN define=BOOST_TEST_NO_MAIN $B2_BUILD_OPTIONS $LIBS_TO_BUILD
+
+# rm -rf bin.v2
+# fi
 
 echo installing boost...
 if [ -d "$BUILD_DIR/frameworks" ]; then
@@ -161,53 +222,62 @@ mkdir "$BUILD_DIR/frameworks"
 
 build_xcframework()
 {
-	xcodebuild -create-xcframework -library stage/macosx/lib/lib$1.a -library stage/catalyst/lib/lib$1.a -library stage/ios/lib/lib$1.a -library stage/iossim/lib/lib$1.a -output "$BUILD_DIR/frameworks/$1.xcframework"
+	xcodebuild -create-xcframework  -library stage/macosx/lib/lib$1.a -library stage/catalyst/lib/lib$1.a -library stage/ios/lib/lib$1.a -library stage/iossim/lib/lib$1.a -output "$BUILD_DIR/frameworks/$1.xcframework"
 }
-
+# -library stage/macosx/lib/lib$1.a -library stage/catalyst/lib/lib$1.a -library stage/ios/lib/lib$1.a
 if true; then
-build_xcframework boost_atomic
-build_xcframework boost_chrono
-build_xcframework boost_container
+# build_xcframework boost_atomic
+# build_xcframework boost_chrono
+# build_xcframework boost_container
 build_xcframework boost_context
-build_xcframework boost_contract
-build_xcframework boost_coroutine
-build_xcframework boost_date_time
-build_xcframework boost_exception
-build_xcframework boost_fiber
+# build_xcframework boost_contract
+# build_xcframework boost_coroutine
+# build_xcframework boost_date_time
+# build_xcframework boost_exception
+# build_xcframework boost_fiber
 build_xcframework boost_filesystem
-build_xcframework boost_graph
-build_xcframework boost_iostreams
-build_xcframework boost_json
-build_xcframework boost_locale
-build_xcframework boost_log
-build_xcframework boost_log_setup
-build_xcframework boost_math_c99
-build_xcframework boost_math_c99l
-build_xcframework boost_math_c99f
-build_xcframework boost_math_tr1
-build_xcframework boost_math_tr1l
-build_xcframework boost_math_tr1f
-build_xcframework boost_nowide
+# build_xcframework boost_graph
+# build_xcframework boost_iostreams
+# build_xcframework boost_json
+# build_xcframework boost_locale
+# build_xcframework boost_log
+# build_xcframework boost_log_setup
+# build_xcframework boost_math_c99
+# build_xcframework boost_math_c99l
+# build_xcframework boost_math_c99f
+# build_xcframework boost_math_tr1
+# build_xcframework boost_math_tr1l
+# build_xcframework boost_math_tr1f
+# build_xcframework boost_nowide
 build_xcframework boost_program_options
-build_xcframework boost_random
+# build_xcframework boost_random
 build_xcframework boost_regex
-build_xcframework boost_serialization
-build_xcframework boost_wserialization
-#build_xcframework boost_stacktrace_addr2line
-build_xcframework boost_stacktrace_basic
-build_xcframework boost_stacktrace_noop
+# build_xcframework boost_serialization
+# build_xcframework boost_wserialization
+# build_xcframework boost_stacktrace_addr2line
+# build_xcframework boost_stacktrace_basic
+# build_xcframework boost_stacktrace_noop
 build_xcframework boost_system
-build_xcframework boost_prg_exec_monitor
-build_xcframework boost_test_exec_monitor
-build_xcframework boost_unit_test_framework
+# build_xcframework boost_prg_exec_monitor
+# build_xcframework boost_test_exec_monitor
+# build_xcframework boost_unit_test_framework
 build_xcframework boost_thread
-build_xcframework boost_timer
-build_xcframework boost_type_erasure
-build_xcframework boost_wave
+# build_xcframework boost_timer
+# build_xcframework boost_type_erasure
+# build_xcframework boost_wave
+
+    # s.vendored_frameworks =
+	# "frameworks/boost_context.xcframework",
+	#  "frameworks/boost_filesystem.xcframework",
+	#  "frameworks/boost_program_options.xcframework",
+	#  "frameworks/boost_regex.xcframework",
+	#   "frameworks/boost_system.xcframework",
+	#   "frameworks/boost_thread.xcframework"
+
 
 mkdir "$BUILD_DIR/frameworks/Headers"
 cp -R boost "$BUILD_DIR/frameworks/Headers/"
-#mv boost "$BUILD_DIR/frameworks/Headers/"
+# mv boost "$BUILD_DIR/frameworks/Headers/"
 touch "$BUILD_DIR/frameworks.built"
 fi
 
